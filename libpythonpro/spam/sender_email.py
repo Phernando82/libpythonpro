d100b921e18b730b16624b41d71ebae740a83dad
Phernando <nandovalverde@gmail.com>
2022-05-02 16:27:17 +0100

    Emulando Teste com Banco de Dados

--- libpythonpro/spam/sender_email.py
+++ libpythonpro/spam/sender_email.py
@@ -1,3 +1,9 @@
 class Sender:
     def send(self, remetente, destinatario, assunto, corpo):
-        return 'nandovalverde@gmail.com'
+        if '@' not in remetente:
+            raise EmailInvalido(f'Email de remetente inválido {remetente}')
+        return remetente
+
+
+class EmailInvalido(Exception):
+    pass
--- libpythonpro/tests/test_spam/test_send_email.py
+++ libpythonpro/tests/test_spam/test_send_email.py
@@ -1,17 +1,40 @@
-from libpythonpro.spam.sender_email import Sender
+import pytest
+
+from libpythonpro.spam.sender_email import Sender, EmailInvalido
 
 
 def test_create_send_email():
     sender = Sender()
     assert sender is not None
 
+@pytest.mark.parametrize(
+    'destinatario',
+    ['fernandoperesvalverde@hotmail.com','nandovalverde@gmail.com']
+)
 
-def test_remetente():
+def test_remetente( destinatario):
     sender = Sender()
+    destinatarios = ['nandovalverde@gmail.com', 'fernandoperesvalverde@hotmail.com']
+    destinatario
     resultado = sender.send(
-        'nandovalverde@gmail.com',
+        destinatario,
         'nandovalverde@gmail.com',
         'Cursos Python Pro',
         'Primeira turma Guido Von Rossum aberta.'
     )
-    assert 'nandovalverde@gmail.com' in resultado
+    assert destinatario in resultado
+
+
+@pytest.mark.parametrize(
+    'remetente',
+    ['','nandovalverde']
+)
+def test_remetente_invalido(remetente):
+    sender = Sender()
+    with pytest.raises(EmailInvalido):
+        sender.send(
+        remetente,
+        'nandovalverde@gmail.com',
+        'Cursos Python Pro',
+        'Primeira turma Guido Von Rossum aberta.'
+    )
